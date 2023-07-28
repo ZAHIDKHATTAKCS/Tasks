f0cbd87e1dcf7fae4adc5b7fb5836ade4d26e521
Muhammad Zahid <zahidkhattakcs@gmail.com>
2023-07-28 09:28:13 -0700

    Today complete the playlist of tailwindcss and create facebook log in page in tailwindcss and learn about how to add custome css to tailwindcss

---
+++ All Tasks in TailwindCSS/Task-8/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1.5' }],
+
+      }
+    },
+  },
+  plugins: [],
+}
+
